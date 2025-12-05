68ab69e46ced55c07dc7f0037e663b0942511121
kingpin27 <anishchaudhary2706@gmail.com>
2025-12-05 10:08:09 +0530

    TEMPLATE 2

--- main.cpp
+++ main.cpp
@@ -26,14 +26,21 @@ const ld EPS = 1e-9;
 
 
 void solve() {
-    
+    int n; cin>>n;
+    cout<<"solve "<< n <<endl;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input","r",stdin);
+	//freopen("output.txt","w",stdout);
+#endif
+
     int tc = 1;
-    // cin >> tc;
+    cin >> tc;
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t << ": ";
         solve();
